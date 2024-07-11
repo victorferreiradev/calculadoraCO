360e05d0f86ba08aad35b6c3f0536f648060a397
Victor Ferreira <husafedj@gmail.com>
2024-07-10 21:31:35 -0300

    Atualização no codigo JS, linpeza dos inputs apos tempo determinado.

--- script.js
+++ script.js
@@ -1,143 +1,159 @@
-        // Função para validar entrada numérica e exibir mensagem de erro se necessário
-        function validarEntradaNumerica(valor, resultadoSpan) {
-            if (isNaN(valor)) {
-                exibirResultado('Por favor, insira um valor numérico válido.', resultadoSpan);
-                return false;
-            }
-            return true;
+// Função para validar entrada numérica e exibir mensagem de erro se necessário
+function validarEntradaNumerica(valor, resultadoSpan) {
+    if (isNaN(valor)) {
+        exibirResultado('Por favor, insira um valor numérico válido.', resultadoSpan);
+        return false;
+    }
+    return true;
+}
+
+// Função para calcular o IVA (Imposto sobre o Valor Adicionado)
+function calcularIVA() {
+    const totalInputIva = document.getElementById('total-inputIva');
+    const resultadoSpan = document.getElementById('resultadoBsIva');
+
+    const valor = parseFloat(totalInputIva.value);
+
+    if (!validarEntradaNumerica(valor, resultadoSpan)) {
+        return;
+    }
+
+    const resultado = valor / 19;
+    resultadoSpan.innerText = `Base IVA: ${resultado.toFixed(2)}`;
+
+    setTimeout(() => {
+        resultadoSpan.innerText = '';
+        const inputs = document.getElementsByTagName('input');
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].value = '';
         }
+        exibirResultado('', resultadoSpan);
+    }, 50000);
+}
 
-        // Função para calcular o IVA (Imposto sobre o Valor Adicionado)
-        function calcularIVA() {
-            const totalInputIva = document.getElementById('total-inputIva');
-            const resultadoSpan = document.getElementById('resultadoBsIva');
+// Função para calcular a Base Não Gravável
+function calcularBaseNaoGravavel() {
+    const totalInput = document.getElementById('total-input');
+    const brutoInput = document.getElementById('bruto-input');
+    const resultadoSpan = document.getElementById('resultadoNGrav');
 
-            const valor = parseFloat(totalInputIva.value);
+    const total = parseFloat(totalInput.value);
+    const bruto = parseFloat(brutoInput.value);
 
-            if (!validarEntradaNumerica(valor, resultadoSpan)) {
-                return;
-            }
+    if (!validarEntradaNumerica(total, resultadoSpan) || !validarEntradaNumerica(bruto, resultadoSpan)) {
+        return;
+    }
 
-            const resultado = valor / 19;
-            resultadoSpan.innerText = `Base IVA: ${resultado.toFixed(2)}`;
+    const resultado = total - bruto;
+    resultadoSpan.innerText = `Base no gravable:  ${resultado.toFixed(2)}`;
 
-            setTimeout(() => {
-                resultadoSpan.innerText = '';
-                exibirResultado('', resultadoSpan);
-            }, 50000);
+    setTimeout(() => {
+        resultadoSpan.innerText = '';
+        const inputs = document.getElementsByTagName('input');
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].value = '';
         }
+        exibirResultado('', resultadoSpan);
+    }, 50000);
+}
 
-        // Função para calcular a Base Não Gravável
-        function calcularBaseNaoGravavel() {
-            const totalInput = document.getElementById('total-input');
-            const brutoInput = document.getElementById('bruto-input');
-            const resultadoSpan = document.getElementById('resultadoNGrav');
+// Função para calcular a Base Impo-Consumo
+function calcularImpoConsumo() {
+    const valorImpoInput = document.getElementById('valorImpo');
+    const resultadoSpan = document.getElementById('resultadoImpo');
 
-            const total = parseFloat(totalInput.value);
-            const bruto = parseFloat(brutoInput.value);
+    const valorImpo = parseFloat(valorImpoInput.value);
 
-            if (!validarEntradaNumerica(total, resultadoSpan) || !validarEntradaNumerica(bruto, resultadoSpan)) {
-                return;
-            }
+    if (!validarEntradaNumerica(valorImpo, resultadoSpan)) {
+        return;
+    }
 
-            const resultado = total - bruto;
-            resultadoSpan.innerText = `Base no gravable:  ${resultado.toFixed(2)}`;
+    const resultado = valorImpo / 8;
+    resultadoSpan.innerText = `Base Impo-Consumo:  ${resultado.toFixed(2)}`;
 
-            setTimeout(() => {
-                resultadoSpan.innerText = '';
-                exibirResultado('', resultadoSpan);
-            }, 50000);
+    setTimeout(() => {
+        resultadoSpan.innerText = '';
+        const inputs = document.getElementsByTagName('input');
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].value = '';
         }
+        exibirResultado('', resultadoSpan);
+    }, 50000);
+}
 
-        // Função para calcular a Base Impo-Consumo
-        function calcularImpoConsumo() {
-            const valorImpoInput = document.getElementById('valorImpo');
-            const resultadoSpan = document.getElementById('resultadoImpo');
+// Função para calcular a Base AIU
+function calcularAiu() {
+    const aiuInput = document.getElementById('valorAiu');
+    const subtotalAiu = document.getElementById('subAiu');
+    const resultadoSpan = document.getElementById('resultadoAiu');
 
-            const valorImpo = parseFloat(valorImpoInput.value);
+    const aiu = parseFloat(aiuInput.value);
+    const subTotal = parseFloat(subtotalAiu.value);
 
-            if (!validarEntradaNumerica(valorImpo, resultadoSpan)) {
-                return;
-            }
+    if (!validarEntradaNumerica(aiu, resultadoSpan) || !validarEntradaNumerica(subTotal, resultadoSpan)) {
+        return;
+    }
 
-            const resultado = valorImpo / 8;
-            resultadoSpan.innerText = `Base Impo-Consumo:  ${resultado.toFixed(2)}`;
+    const resultadoAiu = aiu / 19;
+    const resultadoSub = resultadoAiu - subTotal;
 
-            setTimeout(() => {
-                resultadoSpan.innerText = '';
-                exibirResultado('', resultadoSpan);
-            }, 50000);
+    resultadoSpan.innerText = `Base AIU:  ${resultadoAiu.toFixed(2)} | Base no gravable:  ${resultadoSub.toFixed(2)}`;
+
+    setTimeout(() => {
+        resultadoSpan.innerText = '';
+        const inputs = document.getElementsByTagName('input');
+        for (let i = 0; i < inputs.length; i++) {
+            inputs[i].value = '';
         }
+        exibirResultado('', resultadoSpan);
+    }, 50000);
+}
 
-        // Função para calcular a Base AIU
-        function calcularAiu() {
-            const aiuInput = document.getElementById('valorAiu');
-            const subtotalAiu = document.getElementById('subAiu');
-            const resultadoSpan = document.getElementById('resultadoAiu');
+// Função para exibir resultados na página
+function exibirResultado(mensagem, resultadoSpan, isError = false) {
+    const outputElement = document.getElementById("output");
+    outputElement.innerText = mensagem;
+    outputElement.className = isError ? 'erro' : 'sucesso';
 
-            const aiu = parseFloat(aiuInput.value);
-            const subTotal = parseFloat(subtotalAiu.value);
+    setTimeout(() => {
+        outputElement.innerText = '';
+        outputElement.className = '';
+    }, 3000);
+}
 
-            if (!validarEntradaNumerica(aiu, resultadoSpan) || !validarEntradaNumerica(subTotal, resultadoSpan)) {
-                return;
-            }
 
-            const resultadoAiu = aiu / 19;
-            const resultadoSub = resultadoAiu - subTotal;
+// Função para alternar entre os modos escuro e claro
+function alternarModoEscuro() {
+    const body = document.body;
+    const modoEscuroAtivo = body.classList.toggle('modo-escuro');
 
-            resultadoSpan.innerText = `Base AIU:  ${resultadoAiu.toFixed(2)} | Base no gravable:  ${resultadoSub.toFixed(2)}`;
+    // Altera dinamicamente a imagem do ícone com base no modo escuro
+    let moonIcon = document.getElementById('moonIcon');
 
-            setTimeout(() => {
-                resultadoSpan.innerText = '';
-                exibirResultado('', resultadoSpan);
-            }, 50000);
+    //caso queira fazer alteração do icone posteriormente.
+    if (moonIcon) {
+        if (modoEscuroAtivo) {
+            moonIcon.src = 'https://img.icons8.com/sf-regular-filled/48/moon-symbol.png';
+        } else {
+            moonIcon.src = 'https://img.icons8.com/sf-regular-filled/48/moon-symbol.png';
+
         }
+    }
 
-        // Função para exibir resultados na página
-        function exibirResultado(mensagem, resultadoSpan, isError = false) {
-            const outputElement = document.getElementById("output");
-            outputElement.innerText = mensagem;
-            outputElement.className = isError ? 'erro' : 'sucesso';
+    // Salvar a preferência do usuário no localStorage (opcional)
+    localStorage.setItem('modoEscuro', modoEscuroAtivo);
 
-            setTimeout(() => {
-                outputElement.innerText = '';
-                outputElement.className = '';
-            }, 3000);
-        }
+    // Remova ou ajuste o setTimeout conforme necessário
+    //setTimeout(() => {}, 0); // Alterado o tempo para 0 para executar imediatamente
+}
 
-
-        // Função para alternar entre os modos escuro e claro
-        function alternarModoEscuro() {
-            const body = document.body;
-            const modoEscuroAtivo = body.classList.toggle('modo-escuro');
-
-            // Altera dinamicamente a imagem do ícone com base no modo escuro
-            let moonIcon = document.getElementById('moonIcon');
-
-            //caso queira fazer alteração do icone posteriormente.
-            if (moonIcon) {
-                if (modoEscuroAtivo) {
-                    moonIcon.src = 'https://img.icons8.com/sf-regular-filled/48/moon-symbol.png';
-                } else {
-                    moonIcon.src = 'https://img.icons8.com/sf-regular-filled/48/moon-symbol.png';
-
-                }
-            }
-
-            // Salvar a preferência do usuário no localStorage (opcional)
-            localStorage.setItem('modoEscuro', modoEscuroAtivo);
-
-            // Remova ou ajuste o setTimeout conforme necessário
-            //setTimeout(() => {}, 0); // Alterado o tempo para 0 para executar imediatamente
-        }
-
-        // Inclusão do script JavaScript
-        document.addEventListener("DOMContentLoaded", function() {
-            let docTitle = document.title;
-            window.addEventListener("blur", () => {
-                document.title = "CO - Calculadora";
-            })
-            window.addEventListener("focus", () => {
-                document.title = docTitle;
-            })
-        });
+// Inclusão do script JavaScript
+document.addEventListener("DOMContentLoaded", function() {
+    let docTitle = document.title;
+    window.addEventListener("blur", () => {
+        document.title = "CO - Calculadora";
+    })
+    window.addEventListener("focus", () => {
+        document.title = docTitle;
+    })
+});
